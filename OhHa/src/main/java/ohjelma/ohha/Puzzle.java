eeb32e87195704ba25c9a135604907f691827e7f
Mikael Wide <mikael.wide@helsinki.fi>
2014-02-12 17:02:03 +0200

    ...

--- OhHa/src/main/java/ohjelma/ohha/Puzzle.java
+++ OhHa/src/main/java/ohjelma/ohha/Puzzle.java
@@ -1,6 +1,7 @@
 package ohjelma.ohha;
 
 import java.util.Random;
+import javax.swing.JFrame;
 
 /**
  *
@@ -13,9 +14,16 @@ public class Puzzle {
      * @param args
      */
     public static void main(String[] args) {
-        Random rand = new Random();
-        Ruudukko r = new Ruudukko(4, 4, 3, rand.nextInt(4));
-        TekstiKayttoliittyma txt = new TekstiKayttoliittyma(r);
-        txt.kaynnista();
+//        Random rand = new Random();
+//        Ruudukko r = new Ruudukko(4, 4, 3, rand.nextInt(4));
+//        TekstiKayttoliittyma txt = new TekstiKayttoliittyma(r);
+//        txt.kaynnista();
+//        
+        JFrame window = new JFrame("Sliding Puzzle");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new PeliPaneeli());
+        window.pack();  // finalize layout
+        window.show();  // make window visible
+        window.setResizable(false);
     }
 }
